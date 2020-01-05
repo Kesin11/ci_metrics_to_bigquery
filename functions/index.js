6a25c3788be4a1a5f7f53ef1f33fe90f265cbdfc
Kenta Kase <kesin1202000@gmail.com>
2020-01-05 23:46:57 +0900

    Read GCS data from inside GCF

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
-exports.helloGCSGeneric = (data, context) => {
+const { Storage } = require('@google-cloud/storage');
+
+exports.helloGCSGeneric = async (data, context) => {
   const file = data;
   console.log(`  Event ${context.eventId}`);
   console.log(`  Event Type: ${context.eventType}`);
@@ -11,4 +13,11 @@ exports.helloGCSGeneric = (data, context) => {
   console.log(`  metadata: ${JSON.stringify(file.metadata)}`);
   // このようにアップする
   // gsutil -h x-goog-meta-build-id:11 cp out/functions.xml.json gs://kesin11-junit-bigquery/
+
+  const storage = new Storage()
+  const bucket = storage.bucket('kesin11-junit-bigquery')
+  const uploadedFile = await bucket.file(file.name).download()
+
+  // download()の結果の型はBufferなので、toStringでstringに変換
+  console.log(`  uploadedFile: ${JSON.stringify(uploadedFile.toString())}`)
 };
