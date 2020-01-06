66ac7fae723f5a8613c81c5bce94f8e98dc1237b
Kenta Kase <kesin1202000@gmail.com>
2020-01-07 00:45:57 +0900

    Move core logic to lib

--- index.js
+++ index.js
@@ -1,50 +1,12 @@
 const fs = require('fs')
-const xml2js = require('xml2js')
+const junit2json = require('./lib/junit2json')
 
 const main = async () => {
   const file = process.argv[2]
   // const file = "example/test-output_failure.xml"
 
   const xml = fs.readFileSync(`${__dirname}/${file}`)
-  const result = await xml2js.parseStringPromise(xml, {
-    attrValueProcessors: [xml2js.processors.parseNumbers]
-  })
-  const rawTestsuites = result['testsuites']
-  const output = { ...rawTestsuites['$'] }
-
-  const testsuiteList = []
-  for (const rawTestsuite of rawTestsuites['testsuite']) {
-    const testsuite = { ...rawTestsuite['$'] }
-
-    const testcaseList = []
-    for (const rawTestcase of rawTestsuite['testcase']) {
-      const testcase = { ...rawTestcase['$'] }
-      // TODO: キーの中身を見て$、_、配列であれば展開するように一般化できるはず
-      const rawFailure = rawTestcase['failure']
-      if (rawFailure) {
-        const failureList = []
-        for (const rawFailure of rawTestcase['failure']) {
-          let failure = { ...rawFailure['$'] }
-          if (rawFailure['_']) {
-            failure = { ...failure, body: rawFailure['_'] }
-          }
-          failureList.push(failure)
-        }
-        testcase['failure'] = failureList
-      }
-
-      testcaseList.push(testcase)
-    }
-    testsuite['testcase'] = testcaseList
-    testsuiteList.push(testsuite)
-  }
-  output['testsuite'] = testsuiteList
-
-  // testsuitesへの独自拡張フィールド
-  output['isFailure'] = output['failures'] > 0 ? true : false
-  // もしtestsuite.0.timestampから取得できなければ、GCSのメタデータでもいい
-  output['timestamp'] = output['testsuite'][0]['timestamp'] // dummy
-  output['buildId'] = Math.ceil(Math.random() * 100) // dummy
+  const output = await junit2json.parse(xml)
 
   // console.log(JSON.stringify(output, null, 4))
   console.log(JSON.stringify(output))
