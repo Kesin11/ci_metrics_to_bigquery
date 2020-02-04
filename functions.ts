e174f5b4dfe8dc042d05735c4d51d9182132f29e
Kenta Kase <kesin1202000@gmail.com>
2020-02-05 00:00:03 +0900

    junit2jsonを使うように置き換え

--- functions.ts
+++ functions.ts
@@ -3,7 +3,13 @@ import os from 'os'
 import fs from 'fs'
 import { Storage } from '@google-cloud/storage'
 import { BigQuery } from '@google-cloud/bigquery'
-import * as junit2json from './lib/junit2json'
+import { parse, TestSuites } from 'junit2json'
+
+type ExtendedTestSuites = TestSuites & {
+  allSuccess?: boolean,
+  created?: string,
+  metadata?: { [key: string]: any }
+}
 
 // 同名のファイルをアップロードした場合でもgoogle.storage.object.finalizeはトリガーされる
 // 新規作成と上書きを区別する方法が無いため、新規作成のときだけにフィルタリングは不可能
@@ -26,29 +32,38 @@ const nowISOString = () => {
   return date.toISOString()
 }
 
+// <system-out>, <system-err>のタグは中身が大きい場合があるのでBigQueryに保存させたくない
+const replacer = (key: any, value: any) => {
+  if (key === 'system-out' || key === 'system-err') return undefined
+  return value
+}
+
 // XMLをJSON(+独自フィールド追加）に変換
-const createJunitJson = async (file: any): Promise<object> => {
+const createJunitTestSuites = async (file: any): Promise<ExtendedTestSuites> => {
   const storage = new Storage()
   const bucket = storage.bucket(file.bucket)
   const uploadedXML = await bucket.file(file.name).download()
 
   // download()の結果の型はBufferなので、toStringでstringに変換
-  const convertedJson = await junit2json.parse(uploadedXML.toString())
-  // 独自フィールドの追加
-  convertedJson['allSuccess'] = convertedJson['failures'] === 0 ? true : false
-  // もしtestsuite.0.timestampから取得できなければ、GCF上で起動したときの時間
-  convertedJson['created'] = convertedJson['testsuite'][0]['timestamp'] || nowISOString()
-  convertedJson['metadata'] = file.metadata
+  const testSuites: ExtendedTestSuites = await parse(uploadedXML.toString())
 
-  // console.log(`  convertedJson: ${JSON.stringify(convertedJson, null, 2)}`)
-  return convertedJson
+  // 独自フィールドの追加
+  testSuites['allSuccess'] = testSuites['failures'] === 0 ? true : false
+  // もしtestsuite.0.timestampから取得できなければ、GCF上で起動したときの時間
+  testSuites['created'] = testSuites['testsuite'][0]['timestamp'] || nowISOString()
+  testSuites['metadata'] = file.metadata
+
+  // console.log(`  testSuites: ${JSON.stringify(convertedJson, null, 2)}`)
+  return testSuites
 }
 
 // BigQueryにload
-const loadJunitJson = async (file: any, convertedJson: object) => {
+const loadJunitJson = async (file: any, testSuites: ExtendedTestSuites) => {
   // BigQuery loadのためにjsonを書き出す
   const tempJsonPath = path.join(os.tmpdir(), path.basename(file.name))
-  fs.writeFileSync(tempJsonPath, JSON.stringify(convertedJson))
+  // replacerで余計なキーは削除
+  const convertedJson = JSON.stringify(testSuites, replacer)
+  fs.writeFileSync(tempJsonPath, convertedJson)
 
   const bigquery = new BigQuery()
   const results = await bigquery
@@ -88,6 +103,6 @@ exports.loadJunitToBq = async (data: any, context: any) => {
   // XML以外のファイル、ディレクトリ作成の場合は何もしないで終了
   if (file.contentType !== 'application/xml') return
 
-  const convertedJson = await createJunitJson(file)
-  await loadJunitJson(file, convertedJson)
+  const testSuites = await createJunitTestSuites(file)
+  await loadJunitJson(file, testSuites)
 }
--- index.ts
+++ index.ts
@@ -1,10 +1,21 @@
 import * as fs from 'fs'
 import * as path from 'path'
-import * as junit2json from './lib/junit2json'
+import { parse, TestSuites } from 'junit2json'
+
+type ExtendedTestSuites = TestSuites & {
+  allSuccess?: boolean,
+  created?: string,
+  metadata?: { [key: string]: any }
+}
 
 const nowISOString = () => {
   const date = new Date()
   return date.toISOString()
+}
+
+const replacer = (key: any, value: any) => {
+  if (key === 'system-out' || key === 'system-err') return undefined
+  return value
 }
 
 const main = async () => {
@@ -12,14 +23,14 @@ const main = async () => {
   // const file = "example/test-output_failure.xml"
 
   const xml = fs.readFileSync(path.resolve(file))
-  const output = await junit2json.parse(xml)
+  const output: ExtendedTestSuites = await parse(xml)
 
   // 独自フィールドの追加
   output['allSuccess'] = output['failures'] === 0 ? true : false
   // もしtestsuite.0.timestampから取得できなければ、GCF上で起動したときの時間
   output['created'] = output['testsuite'][0]['timestamp'] || nowISOString()
 
-  // console.log(JSON.stringify(output, null, 4))
-  console.log(JSON.stringify(output))
+  // console.log(JSON.stringify(output, replacer, 2))
+  console.log(JSON.stringify(output, replacer))
 }
 main()
