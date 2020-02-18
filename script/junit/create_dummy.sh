f761d6ddcfe166aa8a55b69a81efaf4fd3f7d471
Kenta Kase <kesin1202000@gmail.com>
2020-02-18 22:35:52 +0900

    .gitignoreミスを修正

---
+++ script/junit/create_dummy.sh
@@ -0,0 +1,31 @@
+# __tests__/gen_dummy_junit/テストを実行してダミーのXMLを生成、GCSにアップするスクリプト
+#
+# junit/の中身を最初に削除し、適当な回数テストを実行してXMLを生成
+# 1つ1つに異なるmetadata(build_id)を付与しつつ、GCSにアップする
+# GCFが正しく発火されれば、BigQueryにまとまった数のダミーデータが登録される
+
+set -u
+
+DUMMY_NUM=1
+BUILD_ID=23
+JOB_NAME=gcf_junit_xml_to_bq
+BUCKET=${BUCKET_PREFIX}-pipeline-metrics-bq # BUCKET_PREFIX from ENV
+
+cd `dirname $0`
+
+rm -f ../junit/*
+for i in `seq 1 ${DUMMY_NUM}`
+do
+  npm run test -- __tests__/gen_dummy_junit
+done
+
+for name in `ls ../junit`
+do
+  echo $name
+  echo "BUILD_ID = ${BUILD_ID}"
+  BUILD_ID=`expr ${BUILD_ID} + 1`
+  gsutil \
+    -h x-goog-meta-build_id:${BUILD_ID} \
+    -h x-goog-meta-job_name:${JOB_NAME} \
+    cp ../../junit/${name} gs://${BUCKET}/junit/
+done
